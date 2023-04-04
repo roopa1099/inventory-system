8c182d989a99adab8cc88967b1b683654ba2ed33
Roopa R~ <roopar2710@gmail.com>
2023-04-04 10:43:44 +0530

    Css fixes

--- web-ui/src/components/header-component/HeaderComponent.js
+++ web-ui/src/components/header-component/HeaderComponent.js
@@ -1,5 +1,5 @@
 
-import { AppBar, Toolbar, Typography, Paper, InputBase, IconButton, makeStyles,  } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Paper, InputBase, IconButton, makeStyles, } from "@material-ui/core";
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 import SearchIcon from "@material-ui/icons/Search";
 import { useState } from "react";
@@ -8,84 +8,84 @@ import React from "react";
 
 const useStyles = makeStyles({
   searchpaper: {
-      marginTop: '10px',
-      marginLeft:'10px',
-      height: '30px',
-      padding: '2px 6px',
-      display: 'flex',
-      alignItems: 'center',
-      width: 200,
-      marginRight: '12px',
-      border: '1px solid  #97A1A9',
-      '& .MuiInputLabel-root':
-      {
-          textAlign: 'left',
-          color: '#97A1A9',
-          opacity: '1',
-      }
-  },
-    input:
+    marginTop: '10px',
+    marginLeft: '10px',
+    height: '30px',
+    padding: '2px 6px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 200,
+    marginRight: '12px',
+    border: '1px solid  #97A1A9',
+    '& .MuiInputLabel-root':
     {
-        color: '#97A1A9',
+      textAlign: 'left',
+      color: '#97A1A9',
+      opacity: '1',
     }
+  },
+  input:
+  {
+    color: '#97A1A9',
+  }
 }
 )
 export default function HeaderComponent(props) {
-  const classes=useStyles();
-  const [searchValue,setSearchValue]=useState("");
+  const classes = useStyles();
+  const [searchValue, setSearchValue] = useState("");
   const displayDesktop = () => {
-    return <Toolbar style={{ backgroundColor: '#1B98F5', width:'1900px', display:'flex'}}>{femmecubatorLogo}</Toolbar>;
+    return <Toolbar style={{ backgroundColor: '#1B98F5', width: '100%' }}>{femmecubatorLogo}</Toolbar>;
   };
 
   const isEmptyOrNot = (value) => {
-    if(value.length == 0)
-    return true;
+    if (value.length == 0)
+      return true;
     return false;
-}
+  }
 
-  const onChangeEvent=(event)=>{
+  const onChangeEvent = (event) => {
     setSearchValue(event.target.value);
-    if(event.target.value==""){
+    if (event.target.value == "") {
       props.searchValue(event.target.value);
     }
 
   }
 
-  const onClickSearch=(event)=>{
+  const onClickSearch = (event) => {
     event.preventDefault();
     props.searchValue(searchValue);
-   
+
   }
 
   const femmecubatorLogo = (
-    <div style={{display: 'contents', justifyContent:'space-between'}}>
+    <div style={{ display: 'flex', width:'100%', justifyContent:'space-between' }}>
       <div>
-    <Typography variant="h4" component="h1" display="flex">
-     <CurrencyRupeeIcon fontSize="30px"/> &nbsp;
-     StockMate
-    </Typography>
+        <Typography variant="h4" component="h1" display="flex">
+          <CurrencyRupeeIcon fontSize="30px" /> &nbsp;
+          StockMate
+        </Typography>
+      </div>
+      <div style={{  paddingBottom: '10px', marginRight:'25px' }}>
+        <Paper component="form" className={classes.searchpaper} >
+          <InputBase className={classes.input} placeholder="Search"
+            onChange={onChangeEvent}
+            inputProps={{ 'aria-label': 'Search', size: 'small', color: 'primary' }} />
+          <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={onClickSearch}>
+            <SearchIcon color="primary" fontSize="small" />
+          </IconButton>
+        </Paper>
+      </div>
     </div>
-     <div style={{paddingLeft:'1210px',paddingBottom: '10px'}}>
-     <Paper component="form" className={classes.searchpaper} >
-       <InputBase className={classes.input} placeholder="Search"
-         onChange={onChangeEvent}
-         inputProps={{ 'aria-label': 'Search', size: 'small', color: 'primary' }} />
-       <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={onClickSearch}>
-         <SearchIcon color="primary" fontSize="small" />
-       </IconButton>
-     </Paper>
-     </div>
-     </div>
   );
 
   return (
     <header backgroundColor='#1B98F5'>
-     
+
       <AppBar>
 
         {displayDesktop()}
       </AppBar>
-     
+
     </header>
   );
 }
